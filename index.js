5a7ef9e89da18332fc0047ba75b9d8cc42ce1bfc
TheYoBots <73843275+TheYoBots@users.noreply.github.com>
2021-04-07 00:03:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express         = require('express');
 const PORT            = process.env.PORT || 5000;
 
 const LidraughtsApi      = require('./api/lidraughts-api');
-const StockFishPlayer = require('./bots/stockfish');
+const ScanEngine = require('./bots/scan-engine');
 const DraughtOTron   = require('./bots/draught-o-tron');
 
 const token           = process.env.API_TOKEN;
