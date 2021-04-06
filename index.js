7831a5b38531fa457b9f03d366c2bc4910c8733a
TheYoBots <elton_nathan@yahoo.com>
2021-04-06 18:26:44 +0000

    Initial Rewrite

---
+++ index.js
@@ -0,0 +1,44 @@
+const express         = require('express');
+const PORT            = process.env.PORT || 5000;
+
+const LidraughtsApi      = require('./api/lidraughts-api');
+const StockFishPlayer = require('./bots/stockfish');
+const DraughtOTron   = require('./bots/draught-o-tron');
+
+const token           = process.env.API_TOKEN;
+
+/**
+ * Start a DraughtOTron (lidraughts account defined by API_TOKEN) that listens for challenges
+ * and spawns games for unrated challenges. A player object must be supplied that can
+ * produce the next move to play given the previous moves.
+ * 
+ * Token can be created on BOT accounts at https://lidraughts.org/account/oauth/token/create
+ * Put the token in the shell environment with
+ * 
+ * export API_TOKEN=xxxxxxxxxxxxxx
+ * yarn install
+ * yarn start
+ * 
+ */
+
+    async function startBot(token, player) {
+        if (token) {
+            const robot = new DraughtOTron(new LidraughtsApi(token), player);
+            const username = (await robot.start()).data.username;
+            return `<a href="https://lidraughts.org/@/${username}">${username}</a> on lidraughts.</h1><br/>`;
+        }
+    }
+
+    async function begin() {
+        var links = "<h1>Challenge:</h1><br/>";
+
+        links += await startBot(token, new Stockfish());
+
+        // heroku wakeup server (not necessary otherwise)
+
+        express()
+            .get("/", (req, res) => res.send(links))
+            .listen(PORT, () => console.log(`draught-o-tron started and listening on ${PORT}`));
+    }
+  
+  begin();
